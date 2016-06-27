826ed0687147ed9811e1e5b15c2082f061ad121d
jaywise <jweiss55@gmail.com>
2016-06-27 14:18:42 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
 console.log('running!');
 
 var app = express();
@@ -63,21 +64,69 @@ module.exports = app;
 //start API calls to OpenEd partner server (http://docs.opened.apiary.io/#reference)
 var request = require('request');
 
-var token = 'a9973d2e38400632e9a6806953f29e766b0bc71d96c1f9559c8f357e72dec995';
+var token = '20263b27cb491cf7f59ed84f5faf7d7cfb32b4deeb1c6e0b4705251ccad4c117';
+
+//algorithm that builds GET url based values from interface
 
 var visualPercentage = 55; 
+var linguisticPercentage = 0;
+var logicalPercentage = 0;
+var resourceType = "";
 
-if (visualPercentage >= 33){
-    request({
+if (visualPercentage >= 33) {
+  resourceType += "video";
+}; 
+if (linguisticPercentage >= 33) {
+  resourceType += "audio";
+}; 
+if (logicalPercentage >= 33) {
+  resourceType += "homework";
+}; 
+
+
+
+//---GETS TOKEN ---
+// request({
+//   method: 'POST',
+//   url: 'https://partner.opened.com/1/oauth/get_token',
+//   headers: {
+//     'Content-Type': 'application/json; charset=utf-8'
+//   },
+//   body: "{  \"client_id\": \"ddf17d90372621bd68210dd3428dcd8e260719a5882a50900a864828e56d9501\",  \"secret\": \"58f50cd0d4491a194576a28df70d6c29cac6b352df68537afa45bd3943eb2e7f\",  \"username\": \"jweiss\"}"
+// }, function (error, response, body) {
+//   console.log('Status:', response.statusCode);
+//   console.log('Headers:', JSON.stringify(response.headers));
+//   console.log('Response:', body);
+// });
+
+
+request({
   method: 'GET',
-  url: 'https://private-anon-c2d294fdb-opened.apiary-mock.com/1/resources.json/?descriptive=World%20War%20II&url=http%3A%2F%2Fyoutube_video.com&limit=10&offset=0&standard_group=2&category=12&standard=K.G.2&area=11&subject=110&grades_range=K-5&grade_group=46&publisher=BrightStorm&resource_type=video&contribution_name=LearningRegistry&license=free%2C%20premium%2C%20paid%2C%20all&schema_org=false&effectiveness=60-80&standard_ids=%5B123%5D&featured=true&embeddable=true',
+  url: 'https://private-anon-c2d294fdb-opened.apiary-proxy.com/1/resources.json/?descriptive=English&limit=3&offset=0&resource_type=video&featured=true&embeddable=true',
   headers: {
     'Content-Type': 'application/json; charset=utf-8',
-    'Authorization': token,
+    'Authorization': 'Bearer fd3b9bb16c867dec58058a32a32bef4d7e69700bbbfc5639cde44e734f253428'
   }}, function (error, response, body) {
   console.log('Status:', response.statusCode);
   console.log('Headers:', JSON.stringify(response.headers));
-  console.log('Response:', body);
-});
+  console.log('Response:', JSON.parse(body));
 
-};
+  var newData = response.body.resources; 
+  console.log("body object:", newData)
+
+  // newData.map(function(obj){
+  //   return obj.share_url; 
+  // });
+
+  // console.log("Body data:", newData);
+
+  // TODO -- map and print arrays of each item with only title and share_url
+
+
+  //reach into body JSON object
+  //grab each share_url 
+  //plug each share_URL into an iframe
+
+});  
+
+
--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sua Sala' });
 });
 
+router.get('/', function(req, res, next) {
+  res.render('index', { link: 'https://www.opened.com/assessment/ratios/1070702'});
+});
+
 router.get('/search_results', function(req, res) {
 	res.send(
 		"Search results page:" 
 	);
 });
 
-router.get('/users', function(req, res) {
-	res.send({
-		users: ['Steve', 'Eric']
-	});
-});
-
 module.exports = router; 
+
+
 
